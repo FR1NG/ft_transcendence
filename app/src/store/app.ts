2ea0cf8b9ef02e9277288bc9a6cfbf4480fa1b30
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-06-26 11:00:11 +0100

    getting the profile data on the app startup if any user is logged

--- app/src/store/app.ts
+++ app/src/store/app.ts
@@ -1,8 +1,23 @@
 // Utilities
 import { defineStore } from 'pinia'
+import { useUserStore } from './user.ts'
+import { useAuthStore } from './auth.ts'
 
 export const useAppStore = defineStore('app', {
   state: () => ({
     //
   }),
+  getters: {
+
+  },
+  actions: {
+    async getInitialData(): Promise<any> {
+      const authStore = useAuthStore();
+      const userStore = useUserStore();
+
+      if(authStore.getLogged) {
+        userStore.getProfile();
+      }
+    }
+  }
 })
