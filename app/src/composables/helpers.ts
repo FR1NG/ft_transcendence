995ae871b207b021f7ba45533283b78d0a78d249
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-07-03 20:44:21 +0100

    adding helper comosables to assign object and empty object
    - helper function that loop over oject entries and set the value to each key as default value based on datatype for primitive types
    - helper function that assign the values of an object to the same keys if exists on another object

---
+++ app/src/composables/helpers.ts
@@ -0,0 +1,24 @@
+
+export function resetObject(object: any) {
+  if(!(object instanceof Object))
+    return;
+  for (const [key, value] of Object.entries(object)) {
+    if(typeof value === 'string')
+      object[key] = '';
+    else if (typeof value === 'number')
+      object[key] = 0;
+    else if (typeof value === 'undefined')
+      object[key] = undefined;
+    else if (typeof value === 'object')
+     resetObject(object[key]);
+    else
+      object[key] = null;
+  }
+}
+
+export function assignObject(src: any, dst: any) {
+  for (const key in src) {
+    if(dst.hasOwnProperty(key))
+      dst[key] = src[key];
+  }
+}
