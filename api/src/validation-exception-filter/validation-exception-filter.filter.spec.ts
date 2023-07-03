7baa47f7878fab55362e2ae0d2c519e85a48c74a
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-07-03 20:40:42 +0100

    adding validation filter to custumize validation errors to be key value pair

---
+++ api/src/validation-exception-filter/validation-exception-filter.filter.spec.ts
@@ -0,0 +1,7 @@
+import { ValidationExceptionFilterFilter } from './validation-exception-filter.filter';
+
+describe('ValidationExceptionFilterFilter', () => {
+  it('should be defined', () => {
+    expect(new ValidationExceptionFilterFilter()).toBeDefined();
+  });
+});
