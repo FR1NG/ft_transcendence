8cd6d65c63c4dc05250f8103bf557585a103a3db
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-07-03 20:48:45 +0100

    changin the login component on router

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -17,7 +17,7 @@ const routes = [
       {
         path: '/login',
         name: 'Login',
-        component: () => import('@/components/LoginComponent.vue')
+        component: () => import('@/views/Login.vue')
       },
       {
         path: '/test',
