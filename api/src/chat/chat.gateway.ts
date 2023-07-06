e160d83113a8cb2a3c81c5377fa5f9c12d05f753
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-07-06 09:00:41 +0100

    emplementing connection and desconnection interfaces for chatGateway

--- api/src/chat/chat.gateway.ts
+++ api/src/chat/chat.gateway.ts
@@ -1,17 +1,30 @@
 import { UseGuards } from '@nestjs/common';
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { WsAuthGuard } from 'src/auth/ws.guard';
 import { UserService } from 'src/user/user.service';
 
 @WebSocketGateway()
-export class ChatGateway {
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
   constructor(private userService: UserService) {}
 
   @WebSocketServer()
   private server;
 
-  @SubscribeMessage('message')
   @UseGuards(WsAuthGuard)
+  handleConnection(client: any, ...args: any[]) {
+    // const { sub } = client.user;
+    // console.log(sub)
+    console.log('from connection handler: ');
+
+  }
+
+
+  handleDisconnect(client: any) {
+    console.log('on disconnect handler');
+  }
+
+  @SubscribeMessage('message')
+  // @UseGuards(WsAuthGuard)
   handleMessage(client: any, payload: any): string {
     client.emit('message', 'hello mohfuckeh client');
     console.log(payload);
@@ -19,12 +32,12 @@ export class ChatGateway {
   }
 
   @SubscribeMessage('connection')
-  @UseGuards(WsAuthGuard)
-  async connectionHandler(client, any, payload: any): Promise<string> {
-    const { sub } = client.user;
-    const resutl = await  this.userService.setOnline(sub, true);
-    client.emit('connected') 
-    client.emit('message', JSON.stringify(resutl));
+  // @UseGuards(WsAuthGuard)
+  async connectionHandler(client: any, payload: any): Promise<string> {
+    // const { sub } = client.user;
+    // const resutl = await  this.userService.setOnline(sub, true);
+    // client.emit('connected') 
+    // client.emit('message', JSON.stringify(resutl));
     return "connected";
   }
 }
