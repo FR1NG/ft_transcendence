d8750b2e31772b2e02032a3caca9acf035b10137
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-07-07 17:23:11 +0100

    sending and deleting friend request on store including actions, states and getters

--- app/src/store/user.ts
+++ app/src/store/user.ts
@@ -6,10 +6,21 @@ export const useUserStore = defineStore('user', {
   state: () => ({
     profile: [],
     loading: true,
-    user: [],
+    user:{
+      friendRequestsSent: [{}],
+      friendRequestsRecieved: [{}]
+    },
+    requestStatus: ''
   }),
   getters: {
-
+    getRequstStatus: state => {
+      if(state.user.friendRequestsSent?.length > 0)
+        return 'recieved';
+      else if(state.user.friendRequestsRecieved?.length > 0)
+        return 'sent';
+      else
+        return 'none';
+    }
   },
   actions: {
     async getProfile(): Promise<any> {
@@ -43,9 +54,46 @@ export const useUserStore = defineStore('user', {
       try {
           const { data } = await axios.get(`/user/filter/?username=${username}`);
           this.user = data;
+          console.log(data)
       } catch(error) {
         console.log(error)
       }
     },
+
+    // send frien request
+    sendFriendRequest(id: string): Promise<any> {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const { data } = await axios.post('/friend',{
+            id
+          });
+          resolve(data);
+          console.log(data)
+          this.user.friendRequestsRecieved[0] = {id: data.id, status: 'sent'}
+        } catch (error) {
+          reject(error);
+        }
+      })
+
+    },
+    // cancel friend request
+    cancelFriendRequest(id: number): Promise<any> {
+      return new Promise(async(resolve, reject) => {
+        try {
+          const result = await axios.delete('/friend', {
+            data: {
+              id
+            }
+          });
+
+          console.log(result);
+          this.user.friendRequestsRecieved = []
+          resolve(result)
+        } catch (error) {
+          console.log(error);
+          reject(error)
+        }
+      })
+    }
   },
 });
