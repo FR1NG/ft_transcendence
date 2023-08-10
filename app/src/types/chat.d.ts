5bb55749377c255bc2424726ac795be6702183b0
Hamza Chakoubi <hchakoub@e2r7p1.1337.ma>
2023-08-10 09:48:27 +0100

    adding conversation type

--- app/src/types/chat.d.ts
+++ app/src/types/chat.d.ts
@@ -1,5 +1,12 @@
+import { User } from "./user"
+
 export type Message = {
   id: string
   type: 'sent' | 'recieved'
   content: string
+}
+
+export type Conversation = {
+  messages: Message[]
+  user: User
 }
