0926ee852641c362ec2ac07159a3dab7c2a2c6a9
Amine EL RHAIR <el.rhair.amine@gmail.com>
2023-08-13 09:07:49 +0100

    modified accountSettings and added a custom divider

--- app/src/components/ProfileAvatar.vue
+++ app/src/components/ProfileAvatar.vue
@@ -1,14 +1,14 @@
-<template>
-  <v-container class="pa-4 text-center">
-    <v-row class="fill-height" align="center" justify="start">
-      <v-col cols="12" md="4">
+<!-- <template>
+  <v-container class="pa-4">
+    <v-row justify="start">
+      <v-col cols="12" md="1">
         <v-hover v-slot="{ isHovering, props }">
-          <v-card rounded="circle" elevation="0" :class="{ 'on-hover': !isHovering }" v-bind="props">
-            <v-avatar cover size="100">
+          <v-card  elevation="0" :class="{ 'on-hover': !isHovering }" v-bind="props">
+            <v-avatar style="position:relative" cover size="80">
               <v-img :src="link">
                 <div class="align-self-right">
                   <v-btn @click="dialog = !dialog" variant="text" :class="{ 'show-btns': isHovering }"
-                    :color="transparent" icon="mdi-image-edit"></v-btn>
+                    color="pink" large style="position:absolute" icon="mdi-image-edit"></v-btn>
                 </div>
               </v-img>
             </v-avatar>
@@ -44,7 +44,7 @@
       </v-form>
     </v-row>
   </v-container>
-</template>
+</template> -->
 
 <script setup lang="ts">
 import { ref } from 'vue';
@@ -64,6 +64,7 @@ const props = defineProps({
     required: true,
   },
 });
+
 
 const handleSubmit = async () => {
   loading.value = true
@@ -89,16 +90,73 @@ const callback = async (response: any) => {
 }
 </script>
 
+<template>
+    <v-card elevation="0">
+      <div class="avatarPic">
+        <img class="pic" :src="link" alt="">
+        <v-btn @click="dialog = !dialog" variant="text" class="changePic"
+        color="gray" small icon="mdi-image-edit"></v-btn>
+      </div>
+      </v-card>
+    <v-row justify="center">
+      <v-form @submit.prevent="handleSubmit" :disabled="loading">
+        <v-dialog v-model="dialog" persistent width="500" backgound="red">
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Update Avatar</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col clos="12">
+                    <v-file-input accept="image/png, image/jpeg, image/bmp" placeholder="Pick an avatar"
+                      prepend-icon="mdi-camera" label="Avatar" v-model="avatar"></v-file-input>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn type="submit" @click="handleSubmit" :loading="loading">
+                update
+              </v-btn>
+              <v-spacer></v-spacer>
+              <v-btn type="submit" @click="dialog=false">
+                cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-form>
+    </v-row>
+</template>
+
+
 <style scoped>
-.v-card {
-  transition: opacity .4s ease-in-out;
+
+.avatarPic {
+  display: grid;
+  grid-template-columns: repeat(16, 1fr);
 }
 
-.v-card:not(.on-hover) {
-  opacity: 0.6;
+.pic {
+  width: 100px;
+  border-radius: 50px;
+  grid-column: 1 / 20;
+  grid-row: 1 / 3;
 }
 
-.show-btns {
-  color: rgba(255, 255, 255, 1) !important;
+.changePic {
+  opacity: 0;
+  grid-column: 2 / 20;
+  grid-row: 2 / 3;
+  transition: 1s;
 }
+
+.pic:hover ~ .changePic,
+.changePic:hover {
+  opacity: 1;
+  /* width: 5px;
+  height: 5px; */
+}
+
 </style>
