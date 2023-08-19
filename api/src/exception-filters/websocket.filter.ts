53100773924f0f1bcb96e213f9683ad7815e6e01
Hamza Chakoubi <hchakoub@c3r10p2.1337.ma>
2023-08-19 16:33:51 +0100

    responding with the same error message when catching the wrong error on wsException filter

--- api/src/exception-filters/websocket.filter.ts
+++ api/src/exception-filters/websocket.filter.ts
@@ -1,10 +1,14 @@
 import { Catch, ArgumentsHost } from '@nestjs/common';
 import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
 
-@Catch()
+@Catch(WsException)
 export class WebSocketExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     if(exception instanceof WsException)
       host.switchToWs().getClient().emit('error', exception.getError());
+    else {
+      const response = host.switchToHttp().getResponse();
+      response.status(exception.response.statusCode).json(exception.response);
+    }
   }
 }
