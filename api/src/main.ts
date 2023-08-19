69cf0d55f32a968fb0307a356fdb4594ab82444b
Hamza Chakoubi <hchakoub@c3r10p2.1337.ma>
2023-08-19 16:35:45 +0100

    adding swagger to document api endpoints

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { PrismaFilter } from './exception-filters/prisma.filter';
 import { WebSocketExceptionFilter } from './exception-filters/websocket.filter';
 import { ValidationExceptionFilterFilter } from './exception-filters/validation.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,11 @@ async function bootstrap() {
   app.useGlobalFilters(new WebSocketExceptionFilter());
   app.useGlobalFilters(new PrismaFilter());
   app.useGlobalFilters(new ValidationExceptionFilterFilter())
+
+  // swagger configuration
+  const swaggerConfig = new DocumentBuilder().setTitle('Pong').setDescription('transcendence project api documentation').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
