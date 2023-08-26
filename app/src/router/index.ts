6a3f905ebce2527d2fa889ef35af26b2022fc152
Hamza Chakoubi <hchakoub@c4r6p6.1337.ma>
2023-08-26 13:20:19 +0100

    requiring authentication on home route

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -13,6 +13,9 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        meta: {
+          auth: true
+        }
       },
       {
         path: '/login',
