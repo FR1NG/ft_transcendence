854f3131b84a7fbc25e8edc64bf51d1eb41bfd52
Amine El rhair <ael-rhai@c3r5p9.1337.ma>
2023-09-07 15:45:08 +0100

    Created Play neon button

---
+++ app/src/components/HomeComponents/Play.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import NeonButton from './NeonButton.vue'
+
+
+</script>
+
+
+
+<template>
+    <div class="playGame">
+        <p>Ready to test your reflexes and paddle your way to victory?</p>
+        <img class="pongGif" src="/images/ppong.gif" alt="pong gif">
+        <NeonButton/>
+    </div >
+</template>
+
+<style lang="scss">
+
+    .playGame {
+        color: rgb(var(--v-theme-secondary));
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        p {
+            margin: 2rem;
+            font-size: 1.2rem;
+        }
+        .pongGif {
+            background-color: red;
+            width: 100%;
+            max-height: 30rem;
+            right: 0;
+            border-radius: 20px;
+            //   margin: 3rem 0 0 5rem;
+        }
+    }
+</style>
