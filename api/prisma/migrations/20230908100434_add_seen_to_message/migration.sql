577a604003c0ff34c4394432f15dad299f6e1ede
Hamza Chakoubi <hchakoub@c4r6p6.1337.ma>
2023-09-08 11:15:24 +0100

    adding seen attribute to message table on prisma schema

---
+++ api/prisma/migrations/20230908100434_add_seen_to_message/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Messages" ADD COLUMN     "seen" BOOLEAN NOT NULL DEFAULT false;
--- api/src/chat/chat.service.ts
+++ api/src/chat/chat.service.ts
@@ -102,6 +102,7 @@ export class ChatService {
               select: {
                 id: true,
                 content: true,
+                seen: true,
                 sender: {
                   select: {
                     id: true,
