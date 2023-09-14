20026b4f1b4544c8a3b83c2d8daf2fa746fcb3e3
Hamza Chakoubi <hchakoub@c4r6p2.1337.ma>
2023-09-14 22:40:06 +0100

    files backup

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,16 +1,25 @@
 import { faker } from '@faker-js/faker'
 
-import { PrismaClient } from '@prisma/client'
+import { GameStatus, Games, PrismaClient, Users } from '@prisma/client'
 const prisma = new PrismaClient()
-async function main() {
+
+function getRandom(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+async function seedUsers() {
+  console.log('creating users');
+  const users: Users[] = [];
   for (let i = 10; i < 20; i++) {
     const user = {
-      username: faker.person.firstName(),
       email: faker.internet.email(),
+      username: faker.person.firstName(),
       avatar: faker.image.avatar(),
-      intra_id: i,
+      intra_id: getRandom(1, 234234434),
     }
-      const record = await prisma.users.upsert({
+    console.log('user to be created')
+    console.log(user)
+    const record = await prisma.users.upsert({
       where: { email: user.email },
       update: {},
       create: {
@@ -20,9 +29,40 @@ async function main() {
         avatar: user.avatar
       },
     });
-    console.log(record)
+    users.push(record);
   }
+  return users;
 }
+
+const seedGames = async (users: Users[]) => {
+ console.log('seeding games')
+  // const statuses: GameStatus[] = ['STARTED', 'FINISHED', 'CEATED'];
+   const games: Games[] = [];
+  const otherId = users[getRandom(1, 10)].id;
+  const ids = [otherId, users[0].id];
+  for (let i = 0; i < 15; i++) {
+    const hostId = ids[getRandom(0,2)];
+    const guestId = ids.indexOf(otherId) === 1 ? ids[0] : ids[1];
+    const record = await prisma.games.create({
+      data: {
+        hostId: hostId,
+        guestId: guestId,
+        status: 'FINISHED',
+        winnerId: users[0].id, 
+        winnerScore: 10,
+        loserScore: getRandom(0, 10)
+      }
+    });
+    games.push(record);
+  }
+  return games;
+}
+
+async function main() {
+  const users = await seedUsers();
+  const games = await seedGames(users);
+}
+
 main()
   .then(async () => {
     await prisma.$disconnect()
--- api/src/invitation/invitation.service.ts
+++ api/src/invitation/invitation.service.ts
@@ -1,11 +1,21 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { Invitation } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import { AuthenticatedUser } from 'src/types';
+import { CreateInvitationDto } from './dto/invitation.dto';
+import { NotificationService } from 'src/notification/notification.service';
 
 @Injectable()
 export class InvitationService {
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService, private notificationService: NotificationService){}
+
+  async createInvitation(user: AuthenticatedUser, data: CreateInvitationDto) {
+    switch (data.type) {
+      case 'FRIEND': return await this.createFrindInvit(user, data);
+      case 'GAME': return await this.createGameInvit(user, data);
+      case 'ROOM': return await this.createRoomInvit(user, data);
+    }
+  }
 
   async getInvitation(user: AuthenticatedUser, id: string) {
     const result = await this.prisma.invitation.findUnique({
@@ -99,5 +109,41 @@ export class InvitationService {
     });
     return result;
   }
+
+  //  create invitation 
+
+  private async createGameInvit(user: AuthenticatedUser, data: CreateInvitationDto) {
+
+  }
+
+  private async createRoomInvit(user: AuthenticatedUser, data: CreateInvitationDto) {
+
+  }
+
+  private async createFrindInvit(user: AuthenticatedUser, data: CreateInvitationDto) {
+    const count = await this.prisma.invitation.count({
+      where: {
+        OR: [
+          { toId: user.sub, byId: data.userId},
+          { byId: user.sub, toId: data.userId},
+        ],
+        type: data.type
+      }
+    });
+    if(count !== 0)
+      throw new ConflictException();
+    const result = await this.prisma.invitation.create({
+      data: {
+        toId: data.userId,
+        byId: user.sub,
+        type: data.type
+      }
+    });
+    if(result)
+      this.notificationService.createNotification(data.userId, `${user.username} sent you a friend request`, `/users/${user.username}`);
+
+    return result;
+  }
+
 
 }
