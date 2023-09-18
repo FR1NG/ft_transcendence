3b5ebe3b00c454c0886101040f3e8a9e2e478901
hamza chakoubi <hamza.chakoubi28@gmail.com>
2023-09-18 20:48:09 +0100

    backup of game work

--- api/src/game/game.module.ts
+++ api/src/game/game.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { GameGateway } from './game.gateway';
+import { PrismaService } from 'src/prisma.service';
+import { InvitationService } from 'src/invitation/invitation.service';
+import { NotificationService } from 'src/notification/notification.service';
+import { UserService } from 'src/user/user.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [GameController],
-  providers: [GameService, GameGateway]
+  providers: [
+    GameService,
+    GameGateway,
+    PrismaService,
+    InvitationService,
+    NotificationService,
+    UserService,
+    ConfigService,
+    JwtService,
+  ]
 })
 export class GameModule {}
