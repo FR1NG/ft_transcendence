75f246d5eeb5d2f9379c31b2c47ca995812ca609
amine el rhair <el.rhair.amine@gmail.com>
2023-09-24 15:23:32 +0100

    Finished bouncing ball component
    -added bouncing ball

--- app/src/components/Bouncing.vue
+++ app/src/components/Bouncing.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div class="wrapper">
+    <div class="bouncingWrapper">
         <div class="paddleWrapper">
             <div class="paddle"><div class="top"></div><div class="bottom"></div><div class="left"></div><div class="right"></div><div class="ceil"></div></div>
         </div>
@@ -10,19 +10,12 @@
 </template>
 
 <style lang="scss" scoped>
-*, *::beforee, *::after {
-    padding: 0;
-    margin: 0 auto;
-    box-sizing: border-box;
-}
-.wrapper {
+.bouncingWrapper {
     position: relative;
-    // background-color: blue;
-    height: 40vw;
-    width: 40vw;
-    margin: 16rem;
-    perspective: 800ox;
-    background-color: green;
+    height: 200px;
+    width: 150px;
+    display: flex;
+    justify-content: center;
 }
 .paddleWrapper {
     position: absolute;
@@ -32,7 +25,7 @@
     display: flex;
     align-items: center;
     justify-content: center;
-    transform: rotateX(70deg);
+    transform: rotateX(80deg);
     transform-style: preserve-3d;
 }
 .paddle {
@@ -58,7 +51,7 @@
     width: 80px;
     height: 80px;
     background: rgb(var(--v-theme-colorTwo)) !important;
-    animation: paddleCeil 5s infinite linear;
+    animation: paddleCeil 2s infinite linear;
 }
 @keyframes paddleCeil {
     0%, 46%, 55%, 100% {
@@ -74,7 +67,7 @@
     background: linear-gradient(to top, rgb(var(--v-theme-colorTwo)), rgb(var(--v-theme-colorThree)));
     width: 80px;
     transform: rotateX(90deg);
-    animation: paddleHeight 5s infinite linear;
+    animation: paddleHeight 2s infinite linear;
 }
 .bottom {
     transform-origin: bottom;
@@ -82,7 +75,7 @@
     width: 80px;
     bottom: 0;
     transform: rotateX(-90deg);
-    animation: paddleHeight 5s infinite linear;
+    animation: paddleHeight 2s infinite linear;
 }
 .left {
     transform-origin: left;
@@ -90,7 +83,7 @@
     height: 80px;
     bottom: 0;
     transform: rotateY(-90deg);
-    animation: paddleWidth 5s infinite linear;
+    animation: paddleWidth 2s infinite linear;
 }
 .right {
     transform-origin: right;
@@ -98,7 +91,7 @@
     height: 80px;
     right: 0;
     transform: rotateY(90deg);
-    animation: paddleWidth 5s infinite linear;
+    animation: paddleWidth 2s infinite linear;
 }
 @keyframes paddleHeight {
     0%, 46%, 55%, 100% {
@@ -114,6 +107,23 @@
     }
     50% {
         width: 10px;
+    }
+}.ball {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background: radial-gradient(circle at top, rgb(var(--v-theme-colorTwo)), rgb(var(--v-theme-colorThree)));
+    animation: bounce 1s linear infinite alternate;
+}
+
+@keyframes bounce {
+    0% {
+        top: 0;
+        animation-timing-function: ease-in;
+    }
+    100% {
+        top: 60%;
     }
 }
 
